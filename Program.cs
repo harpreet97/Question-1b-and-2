64af82b22b9412861930d4e17f1a7523dd2f8abc
harpreet97 <harrycheema97@yahoo.com>
2018-05-02 10:19:22 +1200

    Final commit

--- Program.cs
+++ Program.cs
@@ -28,10 +28,16 @@ namespace Question_2
             Console.WriteLine("***************************************");
             Console.WriteLine("Please enter the colour of the your car?");
             string colour = Console.ReadLine();
+            Console.WriteLine("Colour:" + colour);
+            Console.WriteLine("");
             Console.WriteLine("Please enter the make of the your car?");
             string make = Console.ReadLine();
+            Console.WriteLine("Make:" + make);
+            Console.WriteLine("");
             Console.WriteLine("Please enter the gearbox type of the your car?");
             string gearbox = Console.ReadLine();
+            Console.WriteLine("Gearbox type:" +gearbox);
+            Console.WriteLine("");
             Car obj = new Car(colour, make, gearbox);
             Console.WriteLine(obj.DescribeCar());
         }
